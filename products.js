5a9c5494958bca10bb064e0f6289e1fdebaeeae9
Amol Shrestha <shresthaamol012@gmail.com>
2026-05-01 16:47:28 +0545

    Updated layout, styling improvements and added products page

--- products.js
+++ products.js
@@ -1,76 +1,80 @@
+// /products.js
+
 const products = [
 
 {
 name:"Classic T-Shirt",
-price:"Rs 999",
-image:"ChatGPT Image Apr 1, 2026, 01_32_48 PM.png"
+price:999,
+image:"images/classic-tshirt.png"
 },
 
 {
 name:"Street Hoodie",
-price:"Rs 1999",
-image:"ChatGPT Image Apr 1, 2026, 01_34_24 PM.png"
+price:1999,
+image:"images/street-hoodie.png"
 },
 
 {
 name:"Premium Jacket",
-price:"Rs 2499",
-image:"ChatGPT Image Apr 1, 2026, 01_34_44 PM.png"
+price:2499,
+image:"images/premium-jacket.png"
 },
 
 {
 name:"Casual Shirt",
-price:"Rs 1499",
-image:"ChatGPT Image Apr 1, 2026, 01_35_10 PM.png"
+price:1499,
+image:"images/casual-shirt.png"
 },
 
 {
 name:"Designer Pants",
-price:"Rs 1799",
-image:"ChatGPT Image Apr 1, 2026, 01_36_31 PM.png"
+price:1799,
+image:"images/designer-pants.png"
 },
 
 {
 name:"Fashion Dress",
-price:"Rs 2199",
-image:"ChatGPT Image Apr 1, 2026, 01_37_50 PM.png"
-},
-
-{
-name:"Trendy Sweater",
-price:"Rs 1699",
-image:"ChatGPT Image Apr 1, 2026, 01_49_37 PM.png"
-},
-
-{
-name:"Summer Collection",
-price:"Rs 1299",
-image:"ChatGPT Image Mar 30, 2026, 09_44_18 PM.png"
+price:2199,
+image:"images/fashion-dress.png"
 }
 
-]
+];
 
-const container = document.getElementById("product-container")
 
-let html = ""
+const container =
+document.getElementById(
+"product-container"
+);
+
+
+if(container){
+
+let html="";
+
 products.forEach(product=>{
 
-html += `
+html+=`
 
 <div class="product">
 
-<img src="${product.image}">
+<img
+src="${product.image}"
+alt="${product.name}">
 
 <h3>${product.name}</h3>
 
-<p>${product.price}</p>
+<p>Rs ${product.price}</p>
 
-<button>Order Now</button>
+<button>
+Order Now
+</button>
 
 </div>
 
-`
+`;
 
-})
+});
 
-container.innerHTML = html
+container.innerHTML=html;
+
+}
